196625f5c08f35288f4a5448dbf1e2a6fc68777c
yashas224 <71977329+yashas224@users.noreply.github.com>
2022-03-15 11:40:05 +0530

    update

---
+++ demo-1/src/main/java/com/example/demo1/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.demo1.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+
+    private static final List<Student> STUDENTS = List.of(new Student(1, "yashas"),
+            new Student(2, "malathi"), new Student(3, "Krishna"));
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable(name = "id") int id) {
+        return STUDENTS.stream().filter(s -> s.getStudentId() == id).findFirst().
+                orElseThrow(() -> new IllegalStateException("student with id" + id + "does not exists"));
+    }
+}
